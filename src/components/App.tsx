a81b6d1424539b5098c70003d8b93ec936138e6b
TahiryAllaoui <tallaoui31@gmail.com>
2023-09-30 16:45:54 +0200

    Slide effect operationel

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import '../style/App.scss'
 import About from './About'
 import Header from './Header'
@@ -5,28 +6,33 @@ import Project from './Project'
 import Skills from './Skills'
 
 function App() {
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      console.log(entry);
-      if (entry.isIntersecting) {
-        entry.target.classList.add('show');
-      }
-      else {
-        entry.target.classList.remove('show');
-      }
+  const [nameClass, setNameClass] = useState('');
+  setTimeout(() => {
+    setNameClass('hidden');
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+        }
+        else {
+          entry.target.classList.remove('show');
+        }
+      })
     })
-  })
-
-  const hiddenElements = document.querySelectorAll('.hidden');
-  hiddenElements.forEach((elmts) => observer.observe(elmts));
+  
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((elmts) => observer.observe(elmts));
+  }, 10)
+  
 
   return (
     <div className='app'>
       <div className="line"></div>
       <Header />
-      <About className='hidden' />
-      <Skills className='hidden-skill' />
-      <Project className='hidden' />
+      <About className={nameClass} />
+      <Skills className={nameClass}  />
+      <Project className={nameClass}  />
     </div>
   )
 }
